8c46fe728fe6b96f856cb54606b6f4e7e262b0ce
HesslerN <nicolehessler2021@u.northwestern.edu>
2020-10-06 22:25:55 -0500

    course details screen

--- components/CourseList.js
+++ components/CourseList.js
@@ -6,19 +6,19 @@ import { getCourseNumber, getCourseTerm, hasConflict, terms } from '../utils/cou
 
 
 
-const CourseList = ({courses}) => {
+const CourseList = ({courses, view}) => {
   const [selectedTerm, setSelectedTerm] = useState('Fall');
   const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
 
   return (
     <ScrollView>
       <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
-      <CourseSelector courses={termCourses} />
+      <CourseSelector courses={termCourses} view={view}/>
     </ScrollView>
   );
 };
 
-const CourseSelector = ({courses}) => {
+const CourseSelector = ({courses, view}) => {
   const [selected, setSelected] = useState([]);
   const toggle = course => setSelected(selected => (
   selected.includes(course) ? selected.filter(x => x !== course) : [...selected, course]
@@ -28,7 +28,7 @@ const CourseSelector = ({courses}) => {
     <View style={styles.courseList}>
       {
         courses.map(course => (
-          <Course key={course.id} course={course}
+          <Course key={course.id} course={course} view = {view}
           isDisabled={hasConflict(course, selected)}
           select={toggle}
           isSelected={selected.includes(course)}
