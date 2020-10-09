1ce2bf35a294e4b4031bb500c4eaeba025cd7e3a
HesslerN <nicolehessler2021@u.northwestern.edu>
2020-10-09 04:58:59 -0500

    firebase store

--- screens/CourseEditScreen.js
+++ screens/CourseEditScreen.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
 import Form from '../components/Form';
+import { firebase } from '../firebase';
 
 const Field = ({label, value}) => {
   return (
@@ -15,6 +16,15 @@ const Field = ({label, value}) => {
 
 const CourseEditScreen = ({ navigation, route }) => {
   const course = route.params.course;
+  const [submitError, setSubmitError] = useState('');
+
+  async function handleSubmit(values) {
+    const { id, meets, title } = values;
+    const course = { id, meets, title };
+    firebase.database().ref('courses').child(id).set(course).catch(error => {
+      setSubmitError(error.message);
+    });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -26,6 +36,7 @@ const CourseEditScreen = ({ navigation, route }) => {
             title: course.title,
           }}
           validationSchema={validationSchema}
+          onSubmit={values => handleSubmit(values)}
         >
           <Form.Field
             name="id"
@@ -45,6 +56,8 @@ const CourseEditScreen = ({ navigation, route }) => {
             leftIcon="format-title"
             placeholder="Introduction to programming"
           />
+          <Form.Button title={'Update'} />
+          {<Form.ErrorMessage error={submitError} visible={true} />}
         </Form>
       </ScrollView>
     </SafeAreaView>
